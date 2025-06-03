f804d1580ec8dea1a055bf6d24f20052b92491e4
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2025-06-02 23:26:18 -0600

    ADD item for scraped data from a country

--- WebScraping/items.py
+++ WebScraping/items.py
@@ -3,10 +3,31 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
+from dataclasses import dataclass
 import scrapy
 
-
-class WebscrapingItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+@dataclass
+class CountryItem(scrapy.Item):
+    """
+    Item container for scraped data from 
+    information of a country
+    """
+    # Fields for extracted data
+    name_country : str
+    area : int
+    population : int
+    real_gdp : int
+    unemployment_rate : float
+    taxes : float
+    debt_external : int
+    exchange_rates : float
+    internet_users : int
+    internet_percent : float
+    airports : int
+    merchant_marine : int
+    militar_expenditures : float
+    
+    # Field for extracted flag images
+    image_urls : str
+    images : str
+    image_name : str
--- WebScraping/spiders/country_spider.py
+++ WebScraping/spiders/country_spider.py
@@ -1,14 +1,12 @@
 import scrapy
+from WebScraping.items import CountryItem
 
 from .LinksCountries import GetLinksCountries
 
 class CountrySpider(scrapy.Spider):
     name = 'CountryInformation'
 
-    # start_urls = GetLinksCountries()
-    start_urls = ['https://www.cia.gov/the-world-factbook/countries/']
+    start_urls = GetLinksCountries()
 
     def parse(self,response):
-        for country_link in response.xpath('//a[contains(@class,"inline-link")]/@href'):
-            yield {'name':country_link}
-
+        pass
